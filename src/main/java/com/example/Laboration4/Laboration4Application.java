b7d24c8474127c08ceab48fec17c498e810ca0d7
Shazzad Hossain <m.shazzad@gmail.com>
2023-04-13 14:17:50 +0200

    Initial commit

---
+++ src/main/java/com/example/Laboration4/Laboration4Application.java
@@ -0,0 +1,13 @@
+package com.example.Laboration4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboration4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Laboration4/Laboration4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Laboration4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboration4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
